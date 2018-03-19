0ed002d6201e93fc7fea90b2c50658438105e52f
mhagdorn <magnus.hagdorn@marsupium.org>
2018-03-19 20:43:25 +0000

    allow for multiple channels

--- currentcost.sh
+++ currentcost.sh
@@ -2,14 +2,16 @@
 
 case $1 in
    config)
-        cat <<'EOM'
+	cat <<'EOM'
 graph_title Power Usage
 graph_vlabel power (watt)
 graph_category sensors
-power.label power
 EOM
-        exit 0;;
+	for i in $(seq 1 $(tail -n 1 /home/mhagdorn/CurrentCost/currentcost.data | awk '{print NF-3}')); do
+	    echo p$i.label power s$i
+	done
+
+	exit 0;;
 esac
 
-echo -n "power.value "
-tail -n 1 /home/magi/CurrentCost/currentcost.data | cut -d' ' -f4
+tail -n 1 /home/mhagdorn/CurrentCost/currentcost.data | awk '{for (i=4;i<=NF;i++) printf("p%d.value %s\n",i-3,$i)}'
